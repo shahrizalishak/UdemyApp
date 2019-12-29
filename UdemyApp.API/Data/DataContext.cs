bd2185ca72ce6814040ec3da73ad1379e508ca47
Shah <shah.bankrakyat@gmail.com>
2019-12-30 00:43:15 +0800

    Initial Commit

---
+++ UdemyApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using UdemyApp.API.Models;
+
+namespace UdemyApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
