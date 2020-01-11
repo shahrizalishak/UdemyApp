b80ab62e93d9c35abbd9a2db78fa5136a873cc3a
Shah <shah.bankrakyat@gmail.com>
2020-01-11 21:33:35 +0800

    Login and register

---
+++ UdemyApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { Local } from 'protractor/built/driverProviders';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      console.log('login successfully');
+    }, error => {
+      console.log('Failed to login');
+    });
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('Loged Out');
+  }
+
+
+}
